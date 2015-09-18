4eb290821707249e474de3a76f73225ad2740dcf
Lee A. Butler <iraytrace@gmail.com>
2015-09-18 13:06:37 -0400

    Better About Dialog

--- VSLapp.cpp
+++ VSLapp.cpp
@@ -6,6 +6,7 @@
 #include <QDateTime>
 #include <QMessageBox>
 #include <QMainWindow>
+#include <QTextStream>
 #ifdef __unix
 #include <unistd.h>
 #include <sys/types.h>
@@ -87,22 +88,46 @@ QString VSLapp::getApplicationDir()
     return applicationDir.absolutePath();
 }
 
+#include <QTextEdit>
+class QMessageBoxResize: public QMessageBox
+  {
+  public:
+    QMessageBoxResize(QWidget *parent = 0) : QMessageBox(parent) {
+      setMouseTracking(true);
+      setSizeGripEnabled(true);
+    }
+  private:
+    virtual bool event(QEvent *e) {
+      bool res = QMessageBox::event(e);
+      switch (e->type()) {
+      case QEvent::MouseMove:
+      case QEvent::MouseButtonPress:
+          setSizeGripEnabled(true);
+        setMaximumSize(QWIDGETSIZE_MAX, QWIDGETSIZE_MAX);
+        if (QWidget *textEdit = findChild<QTextEdit *>()) {
+          textEdit->setMaximumHeight(QWIDGETSIZE_MAX);
+        }
+      }
+      return res;
+    }
+  };
 void VSLapp::showAboutDialog(QWidget *parent)
 {
-    QMessageBox msgBox(QMessageBox::Information,
-                       QString("About %1").arg(qApp->applicationName()),
-                       QString("This is <b>%1</b> from <br><b>%2</b>")
-                       .arg(qApp->applicationName())
-                       .arg(qApp->organizationName()),
-                       QMessageBox::Ok,
-                       parent,
-                       Qt::Dialog);
 
-    msgBox.setInformativeText(QString("Built %1").arg(__DATE__ " " __TIME__));
-    msgBox.setDetailedText(QString("Binary:%1").arg(VSLapp::getApplicationDir()));
+    QMessageBoxResize msgBox(parent);
+    QString message;
+    QTextStream stream(&message);
+    stream <<  "Built "  __DATE__ " " __TIME__ << endl
+           << "By " BUILT_BY_USER " on " BUILT_ON_MACHINE << endl;
 
+    msgBox.setInformativeText(message);    msgBox.setDetailedText(QString("Binary:%1").arg(VSLapp::getApplicationDir()));
+    msgBox.setWindowTitle(QString("About %1").arg(qApp->applicationName()));
+    msgBox.setText(QString("This is <b>%1</b> from <br><b>%2</b>")
+                   .arg(qApp->applicationName())
+                   .arg(qApp->organizationName()));
     msgBox.setDefaultButton(QMessageBox::Ok);
     msgBox.exec();
+
 }
 
 #define MainWindowGeometry "MainWindow/geometry"
